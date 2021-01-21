492dcf2d9aed827cda796079b3d7820cdb04027d
Arttu Pennanen <arttu@pennanen.org>
2021-01-21 18:00:16 +0200

    Better login form

--- osa5/bloglist-frontend/src/components/LoginForm.js
+++ osa5/bloglist-frontend/src/components/LoginForm.js
@@ -1,27 +1,32 @@
 import React from 'react'
+import Togglable from './Togglable'
 
-const loginForm = ({ username, password, handleLogin, handleUsernameChange, handlePasswordChange }) => (
-  <form onSubmit={handleLogin}>
-    <div>
-      username
-      <input
-        type="text"
-        value={username}
-        name="Username"
-        onChange={({ target }) => handleUsernameChange(target.value)}
-      />
-    </div>
-    <div>
-      password
-      <input
-        type="password"
-        value={password}
-        name="Password"
-        onChange={({ target }) => handlePasswordChange(target.value)}
-      />
-    </div>
-    <button type="submit">login</button>
-  </form>
-)
+const loginForm = ({ username, password, handleLogin, handleUsernameChange, handlePasswordChange }) => {
+  return (
+    <Togglable buttonLabel="Log in">
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => handleUsernameChange(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => handlePasswordChange(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </Togglable>
+  )
+}
 
 export default loginForm
--- osa5/bloglist-frontend/src/components/Notification.js
+++ osa5/bloglist-frontend/src/components/Notification.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Notification = ({ name, message, type }) => {
   return (
-    <div className={`notification ${type}`}>
-      <p>
-        <span className="name">{name}</span> {message && <span className="message">: {message}</span>}
-      </p>
+    <div className="notificationWrapper">
+      <div className={`notification ${type}`}>
+        <p>
+          <span className="name">{name}</span> {message && <span className="message">: {message}</span>}
+        </p>
+      </div>
     </div>
   )
 }
